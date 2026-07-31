5f137601ed4a29e0967f5fc93704e01c396a7c2a
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2026-07-31 08:39:49 -0300

    Add files via upload

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { router } from 'expo-router';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function Home() {
+
+  const [email, setEmail] = useState('')
+  const [pass, setPass] = useState('')
+
+  const go = () => {
+    router.push('/(tabs)')
+  }
+
   return (
-      <View>
-        <Text>pagina inicial</Text>
+      <View style={s.body}>
+        <View style={s.wrap}>
+            <Text style={s.t}>LOGIN</Text>
+            <Text style={s.t}>Email</Text>
+            <TextInput style={s.input} onChangeText={setEmail} value={email} placeholder='Insira seu email'/>
+            <Text style={s.t}>Senha</Text>
+            <TextInput style={s.input} onChangeText={setPass} value={pass} placeholder='Insira sua senha'/>
+            
+            <View>
+              <TouchableOpacity style={s.button} onPress={go}>
+                  <Text style={s.btnText}>ENTRAR</Text>
+              </TouchableOpacity>
+            </View>
+
+        </View>
       </View>
   );
 }
 
 const s = StyleSheet.create({
- 
+ body:{
+    backgroundColor: '#1f3d99',
+    flex: 1
+ },
+ wrap:{
+    
+ },
+ t:{
+
+ },
+ input:{
+
+ },
+ button:{
+
+ },
+ btnText:{
+
+ }
 });
