54d7e1dd4ae2c90b591c48be5aa0870c09b8b0c5
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2026-07-31 11:04:13 -0300

    Add files via upload

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,4 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageSourcePropType, StyleSheet, Text, View } from 'react-native';
+
+type Curso = {
+  id: string;
+  titulo: string;
+  descricao: string;
+  imagem: ImageSourcePropType | undefined;
+}
+
+const cursos: Curso[]= [
+  {
+    id: '1',
+    titulo: 'Automação Industrial',
+    descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem perferendis, consectetur voluptates voluptatem beatae vero odit nisi tempore doloremque laudantium doloribus aperiam repellat quas omnis corrupti quam quia aliquam magni!',
+    imagem: require('../../assets/01.jpg')
+  },
+  {
+    id: '2',
+    titulo: 'Eletroeletronica',
+    descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, minus laudantium alias deleniti et animi qui expedita, debitis quia illum blanditiis! Nam quo dicta porro adipisci est eum fugit assumenda!',
+    imagem: require('../../assets/02.jpg')
+  },
+  {
+    id: '3',
+    titulo: 'Desenvolvimento de Sistema',
+    descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, minus laudantium alias deleniti et animi qui expedita, debitis quia illum blanditiis! Nam quo dicta porro adipisci est eum fugit assumenda!',
+    imagem: require('../../assets/03.jpg')
+  },
+  {
+    id: '4',
+    titulo: 'Mecatrônica Industrial',
+    descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, minus laudantium alias deleniti et animi qui expedita, debitis quia illum blanditiis! Nam quo dicta porro adipisci est eum fugit assumenda!',
+    imagem: require('../../assets/04.jpg')
+  },
+  {
+    id: '5',
+    titulo: 'Logística',
+    descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, minus laudantium alias deleniti et animi qui expedita, debitis quia illum blanditiis! Nam quo dicta porro adipisci est eum fugit assumenda!',
+    imagem: require('../../assets/05.jpg')
+  },
+]
+
+
 
 export default function TabTwoScreen() {
   return (
